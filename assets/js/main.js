e08a9d18d43bb2eda75946d86af931bac6b9401d
denitze <denise.brachwitz@gmail.com>
2020-11-16 17:39:19 +0100

    missing custom rounds

---
+++ assets/js/main.js
@@ -0,0 +1,78 @@
+
+// HTML 
+// CSS 
+let result = document.getElementById("result");
+let guessUser = document.getElementById("guessField")
+const submitBtn = document.getElementById("guessSubmit")
+let maxRounds = document.getElementsByName("count-rounds");
+let custom = document.getElementById("custom")
+const roundTotal = document.getElementById("roundTotal");
+const roundNow = document.getElementById("roundNow")
+const roundsDiv = document.getElementById("radio-div")
+const roundCount = document.getElementById("round-count")
+let currentRound = 1;
+let maxTries = 0;
+
+// Generate a random number and store it in a variable for later
+let randomNumber = Math.floor(Math.random()*100)+1;
+
+const getRounds = function (e) {
+    if (e.target.id =="custom") {
+        customInput.style.display = "block";
+            customInput.addEventListener("onchange", ()=> {
+                maxTries = e.target.value;
+            })
+        }
+        maxTries = e.target.value;
+        console.log(maxTries);
+    }
+  
+for (const singleRound of maxRounds) {
+    singleRound.addEventListener("click", getRounds);
+  }
+
+//Repeat this until the user is correct OR used up all his attempts 
+// Check in which round we are 
+
+
+// Check to see, if the User number is correct
+// If number is incorrect, check if the number is too low or too high and tell user to try again with a lower/higer number
+
+function main() {
+    submitBtn.addEventListener("click", () => {
+        event.preventDefault()
+        roundNow.innerHTML = currentRound;
+        roundTotal.innerHTML = maxTries;
+        roundsDiv.style.display = "none"
+        roundCount.style.display ="block"
+        console.log(maxTries);
+
+        if (currentRound < maxTries) {
+            if (guessUser.value != randomNumber) {
+                console.log(guessUser.value);
+                console.log(randomNumber);
+                if (guessUser.value < randomNumber){
+                    result.innerHTML +=`${currentRound} - You need to guess higher than ${guessUser.value}, try again!<br>`
+                    console.log("Zu klein");
+                } else if (guessUser.value > randomNumber){
+                    result.innerHTML +=`${currentRound} - You need to guess lower than ${guessUser.value}, try again!<br>`
+                    console.log("Zu groß");
+                }
+            } else {
+                console.log("Richtig");
+                result.innerHTML +=`YOU WIN! <a href="">PLAY AGAIN!</a>`
+                console.log(guessUser.value);
+                console.log(randomNumber);
+            }
+            currentRound++;
+        } else if (currentRound = maxTries) {
+            result.innerHTML +='Oh No! You have lost!! <a href="">TRY AGAIN?</a>'
+        } 
+        }
+        )}
+
+// }
+
+main();
+
+//eine funktion die feststellt wie viele runden schon gespielt sind bzw. wie viele runden noch gespielt werden
